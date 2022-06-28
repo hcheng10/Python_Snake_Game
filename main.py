7e6d033c041dbe0000828f26e717b9c3e2fcc78e
Haowei Cheng <Haowei4671@gmail.com>
2022-06-28 12:59:44 -0700

    finish the game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("Python Snake Game")
 screen.tracer(0) # Turn turtle animation on/off and set delay for update drawings. 0 means turn it off
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,5 +25,22 @@ while game_is_on:
     screen.update() # Perform a TurtleScreen update. To be used when tracer is turned off.
     time.sleep(0.1) # 1 sec delay
     snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15: # Turtle_obj.distance(another_Turtle_obj) to get distance between two Turtle obj
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick() # must put end of the project
--- snake.py
+++ snake.py
@@ -12,14 +12,21 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in START_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position()) # add one segment at the end of snake
 
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1): # make snake body move follow by its head
